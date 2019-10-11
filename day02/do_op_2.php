2a9b9b087710d17dd3ca36759bd406fbe015e2e5
Mthokozisi NZOLO <mnzolo@c5r2s11.wethinkcode.co.za>
2019-10-11 13:09:38 +0200

    all

--- day02/do_op_2.php
+++ day02/do_op_2.php
@@ -1,33 +1,97 @@
 #!/usr/bin/php
 <?php
-    $string = trim($argv[1]);
+    $av = preg_replace('/\s+/', ' ', $argv[1]);
+    $string = trim($av);
     $value = explode(" ", $string);
-    if (count($value) == 1)
+    if ($argc == 1)
     {
-        if(!strpos($value, "+"))
+        echo ("Incorrect Parameters")."\n";
+        exit;
+    }
+    if (count($value) > 1 && count($value) != 3)
+    {
+        foreach ($value as $test)
         {
-            $result = explode("+", $value);
-            echo ($result[0] + $result[1]);
+            if (!(is_numeric($test)))
+            {
+                echo ("Syntax Error")."\n";
+                exit;
+            }
         }
-        if(!strpos($value, "-"))
+    }
+    if (count($value) == 3)
+    {
+        if(!(is_numeric($value[0])) || !(is_numeric($value[2])))
         {
-            $result = explode("-", $value);
-            echo ($result[0] + $result[1]);
+            echo ("Syntax Error")."\n";
+            exit;
         }
-        if(!strpos($value, "*"))
+    }
+    if (count($value) == 1)
+    {   
+        if(strpos($value[0], "+") != false)
         {
-            $result = explode("*", $value);
-            echo ($result[0] + $result[1]);
+            $result = explode("+", $value[0]);
+            if (!(is_numeric($result[0])) || !(is_numeric($result[1])))
+            {
+                echo ("Syntax Error")."\n";
+                exit;
+            }
+            echo ($result[0] + $result[1])."\n";
+            exit;
         }
-        if(!strpos($value, "/"))
+        if(strpos($value[0], "-") != false)
         {
-            $result = explode("/", $value);
-            echo ($result[0] + $result[1]);
+            $result = explode("-", $value[0]);
+            if (!(is_numeric($result[0])) || !(is_numeric($result[1])))
+            {
+                echo ("Syntax Error")."\n";
+                exit;
+            }
+            echo ($result[0] + $result[1])."\n";
+            exit;
+        }
+        if(strpos($value[0], "*") != false)
+        {
+            $result = explode("*", $value[0]);
+            if (!(is_numeric($result[0])) || !(is_numeric($result[1])))
+            {
+                echo ("Syntax Error")."\n";
+                exit;
+            }
+            echo ($result[0] * $result[1])."\n";
+            exit;
+        }
+        if(strpos($value[0], "/") != false)
+        {
+            $result = explode("/", $value[0]);
+            if (!(is_numeric($result[0])) || !(is_numeric($result[1])))
+            {
+                echo ("Syntax Error")."\n";
+                exit;
+            }
+            echo ($result[0] + $result[1])."\n";
+            exit;
+        }
+        else
+        {
+            echo ("Syntax Error")."\n";
+            exit;
         }
     }
     else
     {
-
+        if($value[1] == '+')
+            $result = ($value[0] + $value[2]);
+        if($value[1] == '%')
+            $result = ($value[0] % $value[2]);
+        if($value[1] == '-')
+            $result = ($value[0] - $value[2]);
+        if($value[1] == '/')
+            $result = ($value[0] / $value[2]);
+        if($value[1] == '*')
+            $result = ($value[0] * $value[2]);
+        
+        echo $result."\n";
     }
-    print_r($value);
 ?>
